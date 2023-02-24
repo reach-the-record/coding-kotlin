2778fccfc03878072be8d74fc70a51f99b4e09f4
Chanseong <reachtherecord@gmail.com>
2023-02-24 19:49:40 +0900

    Kotlin Type Any, Unit, Nothing, 컬렉션 배열

--- src/main/kotlin/KotlinType.kt
+++ src/main/kotlin/KotlinType.kt
@@ -174,16 +174,161 @@ data class Person3(val name: String,
 fun main24(args: Array<String>) {
     println(Person3("Sam", 35).isOlderThan(Person3("Amy", 42)))
     println(Person3("Sam", 35).isOlderThan(Person3("Jane")))
-/**
- * 숫자 변환
- * Kotlin은 한 타입의 숫자를 다른 타입의 숫자로 자동 변환하지 않는다.
- * 결과 타입이 허용하는 숫자의 범위가 원래 타입의 범위보다 넓은 경우 조차도 자동 변환은 불가능하다.
- * Kotlin에서 모든 원시 타입에 대한 변환 함수를 제공하므로 직접 변환해야 한다.
- * ex) toByte(), toShort(), toChar(), toLong(), toInt()
- */
+    /**
+     * 숫자 변환
+     * Kotlin은 한 타입의 숫자를 다른 타입의 숫자로 자동 변환하지 않는다.
+     * 결과 타입이 허용하는 숫자의 범위가 원래 타입의 범위보다 넓은 경우 조차도 자동 변환은 불가능하다.
+     * Kotlin에서 모든 원시 타입에 대한 변환 함수를 제공하므로 직접 변환해야 한다.
+     * ex) toByte(), toShort(), toChar(), toLong(), toInt()
+     */
     val i = 1
 //    val l: Long = i // "Error: type mismatch" 컴파일 오류 발생
     val l2: Long = i.toLong()
 
+    /**
+     * Any, Any?
+     * Java - Object == Kotlin - Any
+     * Any는 Int 등 primitive type을 포함한 모든 타입의 최상위 타입
+     */
+    val answer: Any = 42 // Any가 참조 타입이기 때문에 42가 박싱된다.
+}
 
+/**
+ * Unit
+ * Unit - Java의 void와 같은 기능
+ * 반환하지 않는 함수의 반환 타입으로 Unit 사용 가능
+ * 반환 타입 선언 없이 정의한 블록이 본문인 함수와 같다.
+ *
+ * void와 다른점
+ * Unit - 모든 기능을 갖는 일반적인 타입. void와 달리 Unit을 타입 인자로 사용 가능.
+ * Unit 타입에 속한 값은 하나이며, 이름도 Unit. Unit 타입 함수는 Unit값을 묵시적으로 반환.
+ * 제네릭 파라미터를 반환하는 함수를 오버라이드하면서 반환 타입으로 Unit을 쓸 때 유용하다.
+ */
+interface Processor1<T> {
+    fun process() : T
+}
+
+/**
+ * 함수형 프로그래밍에서 전통적으로 Unit은 '단 하나의 인스턴스만 갖는 타입'을 의미한다.
+ * 유일한 인스턴스의 유무가 Java void와 Kotlin Unit을 구분하는 가장 큰 차이다.
+ */
+class NoResultProcessor : Processor1<Unit> {
+    override fun process() { // Unit을 반환하지만 타입을 저장할 필요는 없다.
+        // 업무 처리 코드. 여기서 return을 명시할 필요가 없다.
+    }
+}
+
+/**
+ * Nothing
+ * '반환 값'이라는 개념 자체가 의미 없는 함수가 일부 존재하여 필요한 타입.
+ * Nothing 타입은 아무 값도 포함하지 않는다.
+ * 함수의 반환 타입이나 반환 타입으로 쓰일 타입 파라미터만 쓸 수 있다.
+ * 컴파일러는 Nothing이 반환 타입인 함수가 결코 정상 종료되지 않음을 알고 그 함수를 호출하는 코드를 분석할 때 사용한다.
+ */
+fun main25(args: Array<String>) {
+    fun fail(message: String): Nothing {
+        throw IllegalStateException(message)
+    }
+
+    val company = Company1("comp", Address1("addr", 22, "city", "country"))
+
+    val address = company.address ?: fail("No address")
+    println(address.city)
+
+}
+
+/**
+ *  컬렉션과 배열
+ *  컬렉션 안에 Null을 넣을 수 있는지 여부는 어떤 변수의 값이 Null이 될 수 있는지 여부와 마찬가지로 중요하다.
+ */
+fun addValidNumbers1(numbers: List<Int?>) {
+    var sumOfValidNumbers = 0
+    var invalidNumbers = 0
+    for (number in numbers) {
+        if (number != null) {
+            sumOfValidNumbers += number
+        } else {
+            invalidNumbers++
+        }
+    }
+    println("Sum of valid numbers: $sumOfValidNumbers")
+    println("Invalid numbers: $invalidNumbers")
+}
+
+/**
+ *  리스트의 원소에 접근하면 Int? 타입의 값을 얻는다.
+ *  그 값을 산술식에 사용하기 전에 Null 여부를 검사해야 한다.
+ *  Null이 될 수 있는 값으로 이뤄진 컬렉션으로 Null을 걸러내는 경우가 자주 있어서 코틀린 표준 라이브러리는 그런 일을 하는 filterNotNull이라는 함수를 제공한다.
+ */
+fun addValidNumbers2(numbers: List<Int?>) {
+    val validNumbers = numbers.filterNotNull()
+    println("Sum of valid numbers: ${validNumbers.sum()}")
+    println("Invalid numbers: ${numbers.size - validNumbers.size}")
+}
+
+/**
+ * 읽기 전용과 변경 가능한 컬렉션
+ * Kotlin에서는 Collection 안의 데이터에 접근하는 인터페이스와 컬렉션 안의 데이터를 변경하는 인터페이스를 분리.
+ * 일반적인 읽기 전용 라이브러리 - kotlin.collections.Collection > size, iterator, contains ...
+ * 데이터 수정 라이브러리 - kotlin.collections.MutableCollection > Collection + add, remove, clear ...
+ *
+ * 모든 Kotlin 컬렉션은 그에 상응하는 Java Collection 인터페이스의 인스턴스지만 Kotlin에서는 Java 컬렉션 인터페이스마다 읽기 전용 인터페이스와 변경 가능한 인터페이스를 제공한다.
+ * > Java에서는 컬렉션 구분이 없기 때문에 Kotlin에서 읽기 전용 Collection으로 선언된 객체라도 Java코드에서는 컬렉션 객체의 내용 변경이 가능하다.
+ */
+/*
+    /* Java*/
+    // CollectionUtils.java
+    public class CollectionUtils {
+        public static List<String> uppercaseAll(List<String> items) {
+            for (int i = 0; i < items.size(); i++) {
+                items.set(i, items.get(i).toUpperCase());
+            }
+            return items;
+        }
+    }
+
+    // Kotlin
+    // collections.kt
+    fun printInUppercase(list: List<String>) {
+        println(CollectionUtils.uppercaseAll(list))
+        println(list.first())
+    }
+*/
+
+/**
+ * 객체의 배열과 원시 타입의 배열
+ * Kotlin의 배열 - 타입 파라미터를 받는 클래스
+ * 배열의 원소 타입 - 받는 타입 파라미터에 의해 정해진다.
+ */
+fun main26(args: Array<String>) {
+    for (i in args.indices) {
+        println("Argument $i is: ${args[i]}")
+    }
+}
+
+/**
+ * - 배열을 만드는 방법 -
+ * arrayOf함수에 원소를 넘긴다.
+ * arrayOfNulls 함수에 정수 값을 인자로 넘기면 모든 원소가 null이고 인자로 넘긴 값과 크기가 같은 배열을 만들 수 있다. > 원소 타입이 널이 될 수 있는 타입인 경우에만 이 함수를 쓸 수 있다.
+ * Array 생성자는 배열 크기와 람다를 인자로 받아서 람다를 호출해서 각 배열 원소를 초기화해준다.
+ * arrayOf를 쓰지 않고 각 원소가 널이 아닌 배열을 만들어야 하는 경우 이 생성자를 사용한다.
+ */
+fun main27(args: Array<String>) {
+    val letters = Array<String>(26) { i -> ('a' + i).toString() }
+    println(letters.joinToString(""))
+}
+
+fun main28(args: Array<String>) {
+    val strings = listOf("a", "b", "c")
+    println("%s/%s/%s".format(*strings.toTypedArray()))
+}
+
+fun main29(args: Array<String>) {
+    val squares = IntArray(5) { i -> (i+1) * (i+1) }
+    println(squares.joinToString())
+}
+fun main30(args: Array<String>) {
+    args.forEachIndexed { index, element ->
+        println("Argument $index is: $element")
+    }
 }
